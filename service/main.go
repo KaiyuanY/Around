351a63f3cd9b77ac6e01acef0f178cfee88ed931
Kaiyuan Yu <kaiyuany@usc.edu>
2019-01-12 00:13:09 -0800

    added auth

--- service/main.go
+++ service/main.go
@@ -1,68 +1,87 @@
 package main
+
 import (
-    "context"
-    "fmt"
-    "net/http"
-    "encoding/json"
-    "io"
-    "log"
-    "strconv"
-    "reflect"
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"reflect"
+	"strconv"
 
-    "github.com/olivere/elastic"
-    "github.com/pborman/uuid"
+	"github.com/olivere/elastic"
+	"github.com/pborman/uuid"
 
-    "cloud.google.com/go/storage"
+	"cloud.google.com/go/storage"
+
+	"github.com/gorilla/mux"
+
+	jwtmiddleware "github.com/auth0/go-jwt-middleware"
+	jwt "github.com/dgrijalva/jwt-go"
 )
 
 const (
-    POST_INDEX = "post"
-    POST_TYPE = "post"
-    DISTANCE = "200km"
+	POST_INDEX = "post"
+	POST_TYPE  = "post"
+	DISTANCE   = "200km"
 
-    // Needs to update this URL if you deploy it to cloud.
-    ES_URL = "http://35.236.118.45:9200/"
+	// Needs to update this URL if you deploy it to cloud.
+	ES_URL = "http://35.235.111.237:9200/"
 
-    BUCKET_NAME = "post-images-around-kevin"
+	BUCKET_NAME = "post-images-around-kevin"
 )
-
-
 
 type Location struct {
 	Lat float64 `json:"lat"`
-    Lon float64 `json:"lon"`
+	Lon float64 `json:"lon"`
 }
 
 type Post struct {
-      // `json:"user"` is for the json parsing of this User field. Otherwise, by default it's 'User'.
-	User     string `json:"user"`
-    Message  string  `json:"message"`
-    Location Location `json:"location"`
-    Url      string `json:"url"`
+	// `json:"user"` is for the json parsing of this User field. Otherwise, by default it's 'User'.
+	User     string   `json:"user"`
+	Message  string   `json:"message"`
+	Location Location `json:"location"`
+	Url      string   `json:"url"`
 }
 
 func main() {
-    fmt.Println("started-service")
-    createIndexIfNotExist()
+	fmt.Println("started-service")
+	createIndexIfNotExist()
 
-    http.HandleFunc("/post", handlerPost)
-    http.HandleFunc("/search", handlerSearch)
-    log.Fatal(http.ListenAndServe(":8080", nil))
+	//http.HandleFunc("/post", handlerPost)
+	//http.HandleFunc("/search", handlerSearch)
+	jwtMiddleware := jwtmiddleware.New(jwtmiddleware.Options{
+		ValidationKeyGetter: func(token *jwt.Token) (interface{}, error) {
+			return []byte(mySigningKey), nil
+		},
+		SigningMethod: jwt.SigningMethodHS256,
+	})
+
+	r := mux.NewRouter()
+	r.Handle("/post", jwtMiddleware.Handler(http.HandlerFunc(handlerPost))).Methods("POST")
+	r.Handle("/search", jwtMiddleware.Handler(http.HandlerFunc(handlerSearch))).Methods("GET")
+	r.Handle("/signup", http.HandlerFunc(handlerSignup)).Methods("POST")
+	r.Handle("/login", http.HandlerFunc(handlerLogin)).Methods("POST")
+
+	http.Handle("/", r)
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
 //create elasticsearch instance
-func createIndexIfNotExist(){
-    client, err := elastic.NewClient(elastic.SetURL(ES_URL), elastic.SetSniff(false))
-    if err != nil{
-        panic(err)
-    }
+func createIndexIfNotExist() {
+	client, err := elastic.NewClient(elastic.SetURL(ES_URL), elastic.SetSniff(false))
+	if err != nil {
+		panic(err)
+	}
 
-    exists, err := client.IndexExists(POST_INDEX).Do(context.Background())
-    if err != nil{
-        panic(err)
-    }
-    if !exists {
-        mapping := `{
+	exists, err := client.IndexExists(POST_INDEX).Do(context.Background())
+	if err != nil {
+		panic(err)
+	}
+	if !exists {
+		mapping := `{
             "mappings": {
                 "post": {
                     "properties": {
@@ -73,186 +92,200 @@ func createIndexIfNotExist(){
                 }
             }
         }`
-        _, err = client.CreateIndex(POST_INDEX).Body(mapping).Do(context.Background())
-        if err != nil {
-            panic(err)
-        }
-    }
+		_, err = client.CreateIndex(POST_INDEX).Body(mapping).Do(context.Background())
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	exists, err = client.IndexExists(USER_INDEX).Do(context.Background())
+	if err != nil {
+		panic(err)
+	}
+
+	if !exists {
+		_, err = client.CreateIndex(USER_INDEX).Do(context.Background())
+		if err != nil {
+			panic(err)
+		}
+	}
 }
 
 func handlerPost(w http.ResponseWriter, r *http.Request) {
-      // Parse from body of request to get a json object.
-    fmt.Println("Received one post request")
+	// Parse from body of request to get a json object.
+	fmt.Println("Received one post request")
 
-    w.Header().Set("Content-Type", "application/json")
-    w.Header().Set("Access-Control-Allow-Origin", "*")
-    w.Header().Set("Access-Control-Allow-Headers", "Content-Type,Authorization")
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type,Authorization")
 
-    lat, _ := strconv.ParseFloat(r.FormValue("lat"), 64)
-    lon, _ := strconv.ParseFloat(r.FormValue("lon"), 64)
+	user := r.Context().Value("user")
+	claims := user.(*jwt.Token).Claims
+	username := claims.(jwt.MapClaims)["username"]
 
-    p := &Post{
-        User:    r.FormValue("user"),
-        Message: r.FormValue("message"),
-        Location: Location{
-            Lat: lat,
-            Lon: lon,
-        },
-    }
+	lat, _ := strconv.ParseFloat(r.FormValue("lat"), 64)
+	lon, _ := strconv.ParseFloat(r.FormValue("lon"), 64)
 
-    id := uuid.New()
-    file, _, err := r.FormFile("image")
-    if err != nil {
-        http.Error(w, "Image is not available", http.StatusBadRequest)
-        fmt.Printf("Image is not available %v.\n", err)
-        return
-    }
-    attrs, err := saveToGCS(file, BUCKET_NAME, id)
-    if err != nil {
-        http.Error(w, "Failed to save image to GCS", http.StatusInternalServerError)
-        fmt.Printf("Failed to save image to GCS %v.\n", err)
-        return
-    }
-    p.Url = attrs.MediaLink
+	p := &Post{
+		User:    username.(string),
+		Message: r.FormValue("message"),
+		Location: Location{
+			Lat: lat,
+			Lon: lon,
+		},
+	}
 
-    err = saveToES(p, id)
-    if err != nil {
-        http.Error(w, "Failed to save post to ElasticSearch", http.StatusInternalServerError)
-        fmt.Printf("Failed to save post to ElasticSearch %v.\n", err)
-        return
-    }
-    fmt.Printf("Saved one post to ElasticSearch: %s", p.Message)
+	id := uuid.New()
+	file, _, err := r.FormFile("image")
+	if err != nil {
+		http.Error(w, "Image is not available", http.StatusBadRequest)
+		fmt.Printf("Image is not available %v.\n", err)
+		return
+	}
+	attrs, err := saveToGCS(file, BUCKET_NAME, id)
+	if err != nil {
+		http.Error(w, "Failed to save image to GCS", http.StatusInternalServerError)
+		fmt.Printf("Failed to save image to GCS %v.\n", err)
+		return
+	}
+	p.Url = attrs.MediaLink
+
+	err = saveToES(p, id)
+	if err != nil {
+		http.Error(w, "Failed to save post to ElasticSearch", http.StatusInternalServerError)
+		fmt.Printf("Failed to save post to ElasticSearch %v.\n", err)
+		return
+	}
+	fmt.Printf("Saved one post to ElasticSearch: %s", p.Message)
 }
 
 func handlerSearch(w http.ResponseWriter, r *http.Request) {
-    fmt.Println("Received one request for search")
+	fmt.Println("Received one request for search")
 
-    w.Header().Set("Content-Type", "application/json")
-    w.Header().Set("Access-Control-Allow-Origin", "*")
-    w.Header().Set("Access-Control-Allow-Headers", "Content-Type,Authorization")
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type,Authorization")
 
-    lat, _ := strconv.ParseFloat(r.URL.Query().Get("lat"), 64)
-    lon, _ := strconv.ParseFloat(r.URL.Query().Get("lon"), 64)
-    // range is optional 
-    ran := DISTANCE 
-    if val := r.URL.Query().Get("range"); val != "" { 
-       ran = val + "km" 
-    }
+	lat, _ := strconv.ParseFloat(r.URL.Query().Get("lat"), 64)
+	lon, _ := strconv.ParseFloat(r.URL.Query().Get("lon"), 64)
+	// range is optional
+	ran := DISTANCE
+	if val := r.URL.Query().Get("range"); val != "" {
+		ran = val + "km"
+	}
 
-    fmt.Println("range is ", ran)
+	fmt.Println("range is ", ran)
 
-    posts, err := readFromES(lat, lon, ran)
-    if err != nil {
-        http.Error(w, "Failed to read post from ElasticSearch", http.StatusInternalServerError)
-        fmt.Printf("Failed to read post from ElasticSearch %v.\n", err)
-        return
-    }
+	posts, err := readFromES(lat, lon, ran)
+	if err != nil {
+		http.Error(w, "Failed to read post from ElasticSearch", http.StatusInternalServerError)
+		fmt.Printf("Failed to read post from ElasticSearch %v.\n", err)
+		return
+	}
 
-    js, err := json.Marshal(posts)
-    if err != nil {
-        http.Error(w, "Failed to parse posts into JSON format", http.StatusInternalServerError)
-        fmt.Printf("Failed to parse posts into JSON format %v.\n", err)
-        return
-    }
+	js, err := json.Marshal(posts)
+	if err != nil {
+		http.Error(w, "Failed to parse posts into JSON format", http.StatusInternalServerError)
+		fmt.Printf("Failed to parse posts into JSON format %v.\n", err)
+		return
+	}
 
-    w.Write(js)
+	w.Write(js)
 }
 
 // Save a post to ElasticSearch
 func saveToES(post *Post, id string) error {
-    client, err := elastic.NewClient(elastic.SetURL(ES_URL), elastic.SetSniff(false))
-    if err != nil {
-        return err
-    }
+	client, err := elastic.NewClient(elastic.SetURL(ES_URL), elastic.SetSniff(false))
+	if err != nil {
+		return err
+	}
 
-    _, err = client.Index().
-        Index(POST_INDEX).
-        Type(POST_TYPE).
-        Id(id).
-        BodyJson(post).
-        Refresh("wait_for").
-        Do(context.Background())
-    if err != nil {
-        return err
-    }
+	_, err = client.Index().
+		Index(POST_INDEX).
+		Type(POST_TYPE).
+		Id(id).
+		BodyJson(post).
+		Refresh("wait_for").
+		Do(context.Background())
+	if err != nil {
+		return err
+	}
 
-    fmt.Printf("Post is saved to index: %s\n", post.Message)
-    return nil
+	fmt.Printf("Post is saved to index: %s\n", post.Message)
+	return nil
 }
 
 //read posts from elasticsearch
 func readFromES(lat, lon float64, ran string) ([]Post, error) {
-    client, err := elastic.NewClient(elastic.SetURL(ES_URL), elastic.SetSniff(false))
-    if err != nil {
-        return nil, err
-    }
+	client, err := elastic.NewClient(elastic.SetURL(ES_URL), elastic.SetSniff(false))
+	if err != nil {
+		return nil, err
+	}
 
-    query := elastic.NewGeoDistanceQuery("location")
-    query = query.Distance(ran).Lat(lat).Lon(lon)
+	query := elastic.NewGeoDistanceQuery("location")
+	query = query.Distance(ran).Lat(lat).Lon(lon)
 
-    searchResult, err := client.Search().
-        Index(POST_INDEX).
-        Query(query).
-        Pretty(true).
-        Do(context.Background())
-    if err != nil {
-        return nil, err
-    }
+	searchResult, err := client.Search().
+		Index(POST_INDEX).
+		Query(query).
+		Pretty(true).
+		Do(context.Background())
+	if err != nil {
+		return nil, err
+	}
 
-    // searchResult is of type SearchResult and returns hits, suggestions,
-    // and all kinds of other information from Elasticsearch.
-    fmt.Printf("Query took %d milliseconds\n", searchResult.TookInMillis)
+	// searchResult is of type SearchResult and returns hits, suggestions,
+	// and all kinds of other information from Elasticsearch.
+	fmt.Printf("Query took %d milliseconds\n", searchResult.TookInMillis)
 
-    // Each is a convenience function that iterates over hits in a search result.
-    // It makes sure you don't need to check for nil values in the response.
-    // However, it ignores errors in serialization. If you want full control
-    // over iterating the hits, see below.
-    var ptyp Post
-    var posts []Post
-    for _, item := range searchResult.Each(reflect.TypeOf(ptyp)) {
-        if p, ok := item.(Post); ok {
-            posts = append(posts, p)
-        }
-    }
+	// Each is a convenience function that iterates over hits in a search result.
+	// It makes sure you don't need to check for nil values in the response.
+	// However, it ignores errors in serialization. If you want full control
+	// over iterating the hits, see below.
+	var ptyp Post
+	var posts []Post
+	for _, item := range searchResult.Each(reflect.TypeOf(ptyp)) {
+		if p, ok := item.(Post); ok {
+			posts = append(posts, p)
+		}
+	}
 
-    return posts, nil
+	return posts, nil
 }
 
 //save file(image) to GCS
 func saveToGCS(r io.Reader, bucketName, objectName string) (*storage.ObjectAttrs, error) {
-    ctx := context.Background()
+	ctx := context.Background()
 
-    // Creates a client.
-    client, err := storage.NewClient(ctx)
-    if err != nil {
-        return nil, err
-    }
-    //create a Bucket instance
-    bucket := client.Bucket(bucketName)
-    if _, err := bucket.Attrs(ctx); err != nil {
-        return nil, err
-    }
-    //file should be stored in GCS
-    object := bucket.Object(objectName)
-    wc := object.NewWriter(ctx)
-    if _, err = io.Copy(wc, r); err != nil {
-        return nil, err
-    }
-    if err := wc.Close(); err != nil {
-        return nil, err
-    }
+	// Creates a client.
+	client, err := storage.NewClient(ctx)
+	if err != nil {
+		return nil, err
+	}
+	//create a Bucket instance
+	bucket := client.Bucket(bucketName)
+	if _, err := bucket.Attrs(ctx); err != nil {
+		return nil, err
+	}
+	//file should be stored in GCS
+	object := bucket.Object(objectName)
+	wc := object.NewWriter(ctx)
+	if _, err = io.Copy(wc, r); err != nil {
+		return nil, err
+	}
+	if err := wc.Close(); err != nil {
+		return nil, err
+	}
 
-    if err = object.ACL().Set(ctx, storage.AllUsers, storage.RoleReader); err != nil {
-        return nil, err
-    }
+	if err = object.ACL().Set(ctx, storage.AllUsers, storage.RoleReader); err != nil {
+		return nil, err
+	}
 
+	attrs, err := object.Attrs(ctx)
+	if err != nil {
+		return nil, err
+	}
 
-    attrs, err := object.Attrs(ctx)
-    if err != nil {
-        return nil, err
-    }
-
-    fmt.Printf("Image is saved to GCS: %s\n", attrs.MediaLink)
-    return attrs, nil
+	fmt.Printf("Image is saved to GCS: %s\n", attrs.MediaLink)
+	return attrs, nil
 }
-
